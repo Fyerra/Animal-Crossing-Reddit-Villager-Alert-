ec010265c6a6ab3393115850d929ba54df7eda15
Fyerra <59318486+Fyerra@users.noreply.github.com>
2020-04-28 03:46:07 +0200

    Mit TextArea

--- UserInterface.java
+++ UserInterface.java
@@ -1,24 +1,35 @@
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.concurrent.TimeUnit;
 
 public class UserInterface extends JFrame {
 
-
-
     JTextField field, field2;
     JButton button;
+    JTextArea area;
     static String str;
     static String str2;
     static boolean bool = false;
+    static JButton start;
+    static JTextField begin;
+    static JTextField end;
+    static JTextArea ausgabe;
+
     public UserInterface() {
+
+        JPanel middlePanel = new JPanel ();
+        middlePanel.setBorder ( new TitledBorder ( new EtchedBorder (), "Display Area" ) );
 
         JPanel pan = new JPanel();
         JFrame fr = new JFrame();
 
         field = new JTextField("Villagername", 30);
         field2 = new JTextField("Refreshrate", 30);
+        area = new JTextArea(3, 40);
+        JScrollPane scrollPane = new JScrollPane(area);
+        scrollPane.setBounds(10, 11, 455, 249);
 
 
         setTitle("Animal Crossing - Villager Finder für Reddit");
@@ -26,9 +37,23 @@ public class UserInterface extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        button = new JButton("Start");
+        button.setVisible(true);
+        button.setBounds(100, 100, 200, 50);
+
         pan.add(field);
         pan.add(field2);
+        pan.add(area);
+        pan.add(scrollPane);
+        pan.add(button);
         add(pan);
+
+        area.setRows(5);
+        area.setColumns(25);
+        area.setEditable(false);
+        area.setLineWrap(true);
+        area.setWrapStyleWord(true);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         field.addMouseListener(new MouseListener() {
             @Override
@@ -42,7 +67,7 @@ public class UserInterface extends JFrame {
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                field.setText(JOptionPane.showInputDialog("please enter value"));
+                field.setText(JOptionPane.showInputDialog("Bitte hier Name des gesuchten Bewohners eingeben."));
                 str = field.getText();
             }
 
@@ -70,7 +95,7 @@ public class UserInterface extends JFrame {
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                field2.setText(JOptionPane.showInputDialog("please enter value"));
+                field2.setText(JOptionPane.showInputDialog("Zeit zwischen Prüfungen eingeben (in Sekunden, mindestens 10)."));
                 str2 = field2.getText();
             }
 
@@ -85,19 +110,21 @@ public class UserInterface extends JFrame {
             }
         });
 
-        button = new JButton("Start/Stop");
+  /*      button = new JButton("Start/Stop");
         pan.add(button);
         button.setBounds(240, 100, 60, 40);
-        button.setVisible(true);
+        button.setVisible(true); */
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 bool = !bool;
                 if (bool == false) {
-                    System.out.println("Stopp");
+                    area.append("Stopp\r\n");
+                    button.setText("Start");
                 }
                 else {
-                    System.out.println("Start");
+                    area.append("Start\r\n");
+                    button.setText("Stopp");
                 }
             }
         });
@@ -117,10 +144,13 @@ public class UserInterface extends JFrame {
     }
 
     public int getTextS() {
-         int s = Integer.parseInt(str2);
-         return s;
+        int s = Integer.parseInt(str2);
+        return s;
     }
 
+    public JTextArea getArea() {
+        return area;
+    }
 //    public JButton getButton() {
 //        return button;
 //    }
@@ -128,4 +158,5 @@ public class UserInterface extends JFrame {
     public boolean getBool() {
         return bool;
     }
+
 }
--- main.java
+++ main.java
@@ -10,60 +10,60 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class main {
-
-
     static UserInterface UI = new UserInterface();
     public static void main(String[] args) {
-    String strCopy = "fdklhfklfdsajk";
-    int t = 10;
-    String str = UI.getTextVil();
-    String name = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-    String nameUp = UI.getTextVil().toUpperCase();
-    String nameDo = UI.getTextVil().toLowerCase();;
-    String acvilLast = "hfkladh2456525442lkdfhl";
-    String viltradLast = "hfkladh2456525442lkdfhl";
-    String givLast = "hfkladh2456525442lkdfhl";
-    try {
-        UI.main(args);
-        while (true) {
-            TimeUnit.SECONDS.sleep(1);
-            if (UI.getBool() == true) {
-                while (UI.getBool() == true) {
-                    str = UI.getTextVil();
-                    if (!strCopy.equals(str)) {
-                        acvilLast = "hfkladh2456525442lkdfhl";
-                        viltradLast = "hfkladh2456525442lkdfhl";
-                        givLast = "hfkladh2456525442lkdfhl";
-                    }
-                    try {
-                            if (UI.getTextS() < 10) {
-                                throw new NumberFormatException();
+        String name;
+        String strCopy = "fdklhfklfdsajk";
+        int t = 10;
+        String acvilLast = "hfkladh2456525442lkdfhl";
+        String viltradLast = "hfkladh2456525442lkdfhl";
+        String givLast = "hfkladh2456525442lkdfhl";
+        try {
+            UI.main(args);
+            while (true) {
+                TimeUnit.SECONDS.sleep(1);
+                if (UI.getBool() == true) {
+                    while (UI.getBool() == true) {
+                            String str = UI.getTextVil();
+                            if (!strCopy.equals(str)) {
+                                acvilLast = "hfkladh2456525442lkdfhl";
+                                viltradLast = "hfkladh2456525442lkdfhl";
+                                givLast = "hfkladh2456525442lkdfhl";
                             }
-                            t = UI.getTextS();
-                        } catch (NumberFormatException e) {
-                            System.out.println("Bitte gib eine Zahl größer oder gleich 10 ein. Automatisch auf 10 gesetzt.");
+                            try {
+                                if (UI.getTextS() < 10) {
+                                    throw new NumberFormatException();
+                                }
+                                t = UI.getTextS();
+                            } catch (NumberFormatException e) {
+                                UI.getArea().append("Bitte gib eine Zahl größer oder gleich 10 ein. Automatisch auf 10 gesetzt.\r\n");
+                            }
+                            if (!strCopy.equals(str)) {
+                                strCopy = str;
+                            }
+                            try {
+                                name = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+                            } catch (StringIndexOutOfBoundsException e) {
+                                UI.getArea().append("Gib einen gültigen Text ein.\r\n");
+                                name = "Villagername";
                         }
-                        if (!strCopy.equals(str)) {
-                            strCopy = str;
-                        }
-                        name = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-                        nameUp = UI.getTextVil().toUpperCase();
-                        nameDo = UI.getTextVil().toLowerCase();
-                    acvilLast = acVil(acvilLast, name, nameUp, nameDo);
-                    viltradLast = vilTrad(viltradLast, name, nameUp, nameDo);
-                    givLast = giv(givLast, name, nameUp, nameDo);
-                    TimeUnit.SECONDS.sleep(t);
+                            String nameUp = name.toUpperCase();
+                            String nameDo = name.toLowerCase();
+                        acvilLast = acVil(acvilLast, name, nameUp, nameDo);
+                        viltradLast = vilTrad(viltradLast, name, nameUp, nameDo);
+                        givLast = giv(givLast, name, nameUp, nameDo);
+                        TimeUnit.SECONDS.sleep(t);
+                    }
                 }
             }
-        }
         } catch (IOException e) {
-            System.out.println("Help, an IOException!");
+            UI.getArea().append("Help, an IOException!\r\n");
         } catch (IllegalStateException e) {
-            System.out.println("Something went wrong. A random IllegalStateException appeared!");
+            UI.getArea().append("Something went wrong. A random IllegalStateException appeared!\r\n");
         } catch (InterruptedException e) {
-            System.out.println("Brace yourself, an InterruptedException!");
+            UI.getArea().append("Brace yourself, an InterruptedException!\r\n");
         } //catch (NumberFormatException e) {
-          //  System.out.println("Bitte gib eine Zahl größer oder gleich 10 ein.");
+        //  System.out.println("Bitte gib eine Zahl größer oder gleich 10 ein.");
         //}
     }
 
@@ -76,12 +76,12 @@ public class main {
         for (Element element : allElements) {
             if (element.text().contains(last)) {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACVillager.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACVillager.\r\n");
                 return last;
             }
             else if (element.text().contains(name) || element.text().contains(nameUp) || element.text().contains(nameDo)) {
                 printDate();
-                System.out.println("Neuer Beitrag über " + name + " in r/ACVillager.");
+                UI.getArea().append("Neuer Beitrag über " + name + " in r/ACVillager.\r\n");
                 last = element.text();
                 speicher = last.split("ago");
                 last = speicher[1];
@@ -92,7 +92,7 @@ public class main {
             }
             else {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACVillager.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACVillager.\r\n");
                 return last;
             }
         }
@@ -105,25 +105,25 @@ public class main {
                 viltrad.getAllElements();
         String speicher[];
         for (Element element : allElements) {
-             if (element.text().contains(last)) {
+            if (element.text().contains(last)) {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACNHvillagertrade.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACNHvillagertrade.\r\n");
                 return last;
             }
             else if (element.text().contains(name) || element.text().contains(nameUp) || element.text().contains(nameDo)) {
                 printDate();
-                System.out.println("Neuer Beitrag über " + name + " in r/ACNHvillagertrade.");
-                 last = element.text();
-                 speicher = last.split("ago");
-                 last = speicher[1];
-                 speicher = last.split("•");
-                 last = speicher[0];
+                UI.getArea().append("Neuer Beitrag über " + name + " in r/ACNHvillagertrade.\r\n");
+                last = element.text();
+                speicher = last.split("ago");
+                last = speicher[1];
+                speicher = last.split("•");
+                last = speicher[0];
                 Toolkit.getDefaultToolkit().beep();
                 return last;
             }
             else {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACNHvillagertrade.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACNHvillagertrade.\r\n");
                 return last;
             }
         }
@@ -138,12 +138,12 @@ public class main {
         for (Element element : allElements) {
             if (element.text().contains(last)) {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.\r\n");
                 return last;
             }
             else if (element.text().contains(name) || element.text().contains(nameUp) || element.text().contains(nameDo)) {
                 printDate();
-                System.out.println("Neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.");
+                UI.getArea().append("Neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.\r\n");
                 last = element.text();
                 speicher = last.split("ago");
                 last = speicher[1];
@@ -154,7 +154,7 @@ public class main {
             }
             else {
                 printDate();
-                System.out.println("Kein neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.");
+                UI.getArea().append("Kein neuer Beitrag über " + name + " in r/ACNHVillagerGiveaways.\r\n");
                 return last;
             }
         }
@@ -164,8 +164,6 @@ public class main {
     public static void printDate() {
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         Date date = new Date();
-        System.out.print(formatter.format(date) + ": ");
+        UI.getArea().append(formatter.format(date) + ": ");
     }
-
 }
-
